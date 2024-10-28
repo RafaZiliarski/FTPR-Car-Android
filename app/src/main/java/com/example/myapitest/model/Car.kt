9f8e1204e9c1a15b4aebb78e013fea86ad57b8fa
rziliarski <rafaziliarski@gmail.com>
2024-10-28 10:12:36 -0300

    Adicionado modelo do objeto Carro para a estrutura json esperada.

---
+++ app/src/main/java/com/example/myapitest/model/Car.kt
@@ -0,0 +1,21 @@
+package com.example.myapitest.model
+
+data class Car(
+    val id: String,
+    val value: CarValue
+)
+
+data class CarValue(
+    val id: String,
+    val imageUrl: String,
+    val year: String,
+    val name: String,
+    val licence: String,
+    val place: CarPlace?
+)
+
+data class CarPlace(
+    val name: String,
+    val latitude: Double,
+    val longitude: Double
+)
